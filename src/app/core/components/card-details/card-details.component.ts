de0dfe98a97d7510e8905a0f78e3413121228898
Sonam Choeda <sonamchoeda@selise.ch>
2022-06-01 01:21:05 +0600

    card details

--- src/app/core/components/card-details/card-details.component.ts
+++ src/app/core/components/card-details/card-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CardCreationComponent } from '../post-details/card-creation/card-creation.component';
 
 @Component({
   selector: 'app-card-details',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardDetailsComponent implements OnInit {
 
-  constructor() { }
+  cardId: any;
+  constructor(private route: ActivatedRoute, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.cardId = this.route.snapshot.params['id'];
   }
 
+  editCard() {
+    this.dialog.open(CardCreationComponent, {
+      width: '600px',
+      data: this.cardId,
+       autoFocus: false
+    }).afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
+
+  deleteCard() {
+    alert('delete this card');
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
